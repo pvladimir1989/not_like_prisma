ffa3da131bf9e2c8c3361c0f2bc81cd2a141e48b
Vladimir <vpokutniy@inbox.ru>
2020-10-10 11:55:46 +0300

    buttons1

--- not_like_prisma/utils.py
+++ not_like_prisma/utils.py
@@ -1,9 +1,11 @@
 import os
 from filters import ImageFilters
 
+
 def make_folders():
     os.makedirs('input', exist_ok=True)  # добавляется папка input
     os.makedirs('output', exist_ok=True)  # добавляется папка output
+
 
 def save_user_image(user_photo):
     make_folders()
@@ -17,6 +19,3 @@ def filter_user_image(image, filter_attribute):
     filter = ImageFilters(image)
     edited_photo = getattr(filter, filter_attribute)
     return edited_photo()
-
-
-
