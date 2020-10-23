ebd31ad3c82f2d50d6930b06c123c3a8841d8549
Kirill Klemazov <klemazov_kirill@mail.ru>
2020-10-23 07:33:14 +0300

    change run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-export FLASK_APP=not_like_prisma && export FLASK_ENV=development && flask run
+export FLASK_APP=not_like_prisma && export FLASK_ENV=development && flask run && export PYTHONPATH=$PYTHONPATH:not_like_prisma
