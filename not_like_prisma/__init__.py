51f068615486c1b5c9e70980b91671a2cccd1798
pvladimir1989 <vpokutniy@inbox.ru>
2020-11-02 17:24:19 +0300

    error in buttons

--- not_like_prisma/__init__.py
+++ not_like_prisma/__init__.py
@@ -10,6 +10,8 @@ from not_like_prisma.filters import ImageFilters
 
 #
 from not_like_prisma.settings import UPLOAD_FOLDER, ALLOWED_EXTENSIONS
+
+
 # from not_like_prisma.utils import filter_user_image
 
 
@@ -18,56 +20,89 @@ def create_app():
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
     def allowed_file(filename):
-        print('.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
-        return '.' in filename and \
-               filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    # @app.route('/', methods=['GET', 'POST'])
+    # def what_filter():
+    # if request.method == 'POST':
+    #     if 'file' not in request.files:
+    #         flash('No file part')
+    #         return redirect(request.url)
+    #     file = request.files['file']
+    #     # if user does not select file, browser also
+    #     # submit an empty part without filename
+    #     if file.filename == '':
+    #         flash('No selected file')
+    #         return redirect(request.url)
+    #     if file and allowed_file(file.filename):
+    #         filename = secure_filename(file.filename)
+    #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #         image = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    #         filter = ImageFilters(image).gray_filter()
+    #         cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), filter)
+    #         print(redirect(url_for('uploaded_file', filename=filename)))
+    #         return redirect(url_for('uploaded_file', filename=filename))
+
+    # return render_template('index.html')
 
     @app.route('/', methods=['GET', 'POST'])
     def upload_file():
         if request.method == 'POST':
             # check if the post request has the file part
-            if 'file' not in request.files:
-                flash('No file part')
-                return redirect(request.url)
-            file = request.files['file']
-            print(file)
-            # if user does not select file, browser also
-            # submit an empty part without filename
-            if file.filename == '':
-                flash('No selected file')
-                return redirect(request.url)
-            if file and allowed_file(file.filename):
-                filename = secure_filename(file.filename)
-                print(filename)
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                image = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                filter = ImageFilters(image).gray_filter()
-                # gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                # ImageFilters.gray_filter(image)
-                cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), filter)
-                # print(gray_image)
-                # edited_photo = filter_user_image(filter, "gray_filter")
-                # cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), edited_photo)
+            if request.form['increase_brightness'] == 'increase_brightness':
+                # what_filter()
+                if 'file' not in request.files:
+                    flash('No file part')
+                    return redirect(request.url)
+                file = request.files['file']
+                if file.filename == '':
+                    flash('No selected file')
+                    return redirect(request.url)
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    image = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    filter = ImageFilters(image).increase_brightness()
+                    cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), filter)
+                    return redirect(url_for('uploaded_file', filename=filename))
 
-                print(file)
-                print(redirect(url_for('uploaded_file', filename=filename)))
-                return redirect(url_for('uploaded_file', filename=filename))
+            elif request.form['gray'] == 'gray':
+                if 'file' not in request.files:
+                    flash('No file part')
+                    return redirect(request.url)
+                file = request.files['file']
+                if file.filename == '':
+                    flash('No selected file')
+                    return redirect(request.url)
+                if file and allowed_file(file.filename):
+                    filename = secure_filename(file.filename)
+                    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    image = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    filter = ImageFilters(image).gray_filter()
+                    cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), filter)
+                    return redirect(url_for('uploaded_file', filename=filename))
 
         return render_template('index.html')
 
-
-
     @app.route('/uploads/<filename>')
     def uploaded_file(filename):
-        # print(send_from_directory(app.config['UPLOAD_FOLDER'], filename))
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-
+    #
+    # @app.route('/', methods=['GET', 'POST'])
+    # def contact():
+    #     if request.method == 'POST':
+    #         if request.form['gray'] == 'gray':
+    #             print("ok") # do something
+    #             render_template('index.html')
+    #         elif request.form['submit_button'] == 'Do Something Else':
+    #             pass  # do something else
+    #         else:
+    #             pass  # unknown
+    #     elif request.method == 'GET':
+    #         return render_template('index.html')
 
     from werkzeug.middleware.shared_data import SharedDataMiddleware
     app.add_url_rule('/uploads/<filename>', 'uploaded_file', build_only=True)
     app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {'/uploads': app.config['UPLOAD_FOLDER']})
     return app
-
-
-
--- not_like_prisma/filters_for_flask.py
+++ not_like_prisma/filters_for_flask.py
@@ -1,4 +1,4 @@
 import cv2
 
-def filters(path,filename):
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
